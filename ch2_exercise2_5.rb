d583a4726c6b8706e09a3dfa0b5a84cc4767a99e
hakimu jackson <hakimuj@gmail.com>
2013-06-15 12:55:57 -0700

    initial commit

---
+++ ch2_exercise2_5.rb
@@ -0,0 +1,18 @@
+ #Hours in a year
+ hours_day = 24
+ days_year = 365
+ hours_year = hours_day * days_year
+ puts hours_year
+
+ #Minutes in a decade
+ minutes_day = 1440
+ days_decade = 3650
+ minutes_decade = minutes_day * days_decade
+ puts minutes_decade
+
+#how old is Chris Pine
+ age_in_seconds = 1111000000
+ seconds_per_year = 31536000
+ author_age = age_in_seconds / seconds_per_year
+ puts author_age
+ 
